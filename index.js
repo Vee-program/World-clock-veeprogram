908df0c49fb89644c0444561e5bc2b6175ca32be
Vee-program <masilelathembelihle44@gmail.com>
2024-09-17 16:09:30 +0200

    added a moment library for timezone

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ function updateTime() {
   mumbaiDateElement.innerHTML = moment().format("MMMM Do YYYY");
 }
 setInterval(updateTime, 100);
+
+function updateCityTimeZone(event) {
+  let cityTimeZone = event.target.value;
+  let cityZone = event.target.value.replace("_", " ").split("/")[1];
+  let timeZone = moment()
+    .tz(cityTimeZone)
+    .format("h:mm:ss [<small>]A[</small>]");
+  let dateElement = moment().tz(`${event.target.value}`).format("MMMM Do YYYY");
+
+  let citiesContainer = document.querySelector(".cities-container");
+  citiesContainer.innerHTML = `<div class="city" id="pretoria">
+          <h2 class="pretoria">${cityZone}</h2>
+          <span class="date">${dateElement.format("MMMM Do YYYY")}</span>
+          <span class="time">${timeZone.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</span>
+          <hr />
+        </div>`;
+  alert(timeZone);
+}
+let selectCity = document.querySelector("#city");
+selectCity.addEventListener("change", updateCityTimeZone);
